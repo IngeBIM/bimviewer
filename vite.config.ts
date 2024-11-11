be82ea420ebe927a3f027b5739ef2d953bb01763
Felipe Jerez <fjerez034@gmail.com>
2024-11-10 22:14:26 -0300

    Consolidated package.json and vite.config.ts configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./",
+  base: "./",                      // Ruta base relativa
+  plugins: [react()],              // Plugin para soporte de React
   esbuild: {
     supported: {
-      "top-level-await": true,
+      "top-level-await": true      // Soporte para await en el nivel superior
     },
   },
 });
